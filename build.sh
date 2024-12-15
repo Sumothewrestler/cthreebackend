685971959718ee5b5a229aa6a581d0541a04ff84
Aravindhan <personalflareup@gmail.com>
2024-12-15 18:34:48 +0530

    6

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
-# Install python dependencies
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Create static directory
+mkdir -p static
 
 # Collect static files
 python manage.py collectstatic --no-input
