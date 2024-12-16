a6add704bf4f18a8ad4978aeeaf47de9ded21fd0
Aravindhan <personalflareup@gmail.com>
2024-12-16 06:30:27 +0530

    22

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,5 @@
 import os
 
-bind = "0.0.0.0:8000"  # Explicitly set to port 8000
 workers = 4
 worker_class = 'gthread'
 threads = 4
--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export PORT=8000
-exec gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application
+exec gunicorn --bind "0.0.0.0:$PORT" myproject.wsgi:application
